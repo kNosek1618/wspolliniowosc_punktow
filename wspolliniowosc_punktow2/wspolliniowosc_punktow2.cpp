e656eb1f13219d82c8a4866686f5c384a3076e50
kNosek1618 <k.nosek@ievent-management.pl>
2019-05-27 15:13:59 +0200

    add code_2

--- wspolliniowosc_punktow2/wspolliniowosc_punktow2.cpp
+++ wspolliniowosc_punktow2/wspolliniowosc_punktow2.cpp
@@ -1,1 +1,26 @@
-﻿
+﻿#include <iostream>
+#include <math.h>
+#include <cmath>
+#include "pch.h"
+
+using namespace std;
+
+float xA, yA, xB, yB, y, x;
+
+int main()
+{
+	cout << "wspolliniowosc  punktow" << endl;
+
+	yA = 2;
+	xA = 1;
+	yB = 4;
+	xB = 3;
+
+	(y - yA = 1);
+
+	cout << y;
+
+
+
+	return 0;
+}
